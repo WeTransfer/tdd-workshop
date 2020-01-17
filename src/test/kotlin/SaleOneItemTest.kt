6757b7201189acea8b57eb98f540441536fcd668
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-01-17 18:51:38 +0100

    Added Display dependency to Sale

--- src/test/kotlin/SaleOneItemTest.kt
+++ src/test/kotlin/SaleOneItemTest.kt
@@ -1,4 +1,5 @@
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 
 class SaleOneItemTest {
@@ -6,22 +7,41 @@ class SaleOneItemTest {
     @Test
     fun productFound() {
         val display = Display()
-        val sale = Sale()
+        val sale = Sale(display)
 
         sale.onBarcode("12345")
 
         assertEquals("$5.50", display.getText())
     }
+
+    @Test
+    @Ignore
+    fun anotherProductFound() {
+        val display = Display()
+        val sale = Sale(display)
+
+        sale.onBarcode("23456")
+
+        assertEquals("$7.99", display.getText())
+    }
 }
 
-class Sale {
+class Sale(private val display: Display) {
+
     fun onBarcode(barcode: String) {
+
+        display.setText("$5.50")
     }
 }
 
 class Display {
+    private var text = ""
+
     fun getText(): String {
-        return "$5.50"
+        return text
     }
 
+    fun setText(text: String) {
+        this.text = text
+    }
 }
